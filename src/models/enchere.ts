08be6dc8511f5ba3e60bb70c8c39f46dfc590008
Etheve Samuel <etheve.samuel@gmail.com>
2017-09-13 10:48:15 +0400

    test2

---
+++ src/models/enchere.ts
@@ -0,0 +1,13 @@
+export class Enchere {
+public id: string;
+public current_price: number;
+public status: string;
+
+constructor(id: string, current_price: number, status: string){
+  this.id = id;
+  this.current_price = current_price;
+  this.status = status;
+
+}
+
+}
